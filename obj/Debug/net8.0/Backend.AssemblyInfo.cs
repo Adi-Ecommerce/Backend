5b9d5524bd1a59fcac74e9b1af23b7929bc558e3
Adeyinka <adeyinkaadediran72@gmail.com>
2025-10-04 06:23:28 -0700

    migration added

--- obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a968c89489400732b1a58dfc0eac75834ac0ca0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8d3f4e914730dbccca945166524dbf867a941de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
